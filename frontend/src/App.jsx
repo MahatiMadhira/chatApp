ba8ca61b83d48a0c2cdb43c14d3b1afb6699eb2a
MahatiMadhira <93003959+MahatiMadhira@users.noreply.github.com>
2023-12-18 14:59:36 -0500

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+/* RENDER AUTH PAGE IF USER DOES NOT EXIST, RENDER CHAT PAGE IF USER EXISTS */
+import { useState } from "react";
+
+import "./App.css";
+
+import AuthPage from "./AuthPage";
+import ChatsPage from "./ChatsPage";
+
+function App() {
+  const [user, setUser] = useState(undefined);
+
+  if (!user) {
+    return <AuthPage onAuth={(user) => setUser(user)} />;
+  } else {
+    return <ChatsPage user={user} />;
+  }
+}
+
+export default App;
+
